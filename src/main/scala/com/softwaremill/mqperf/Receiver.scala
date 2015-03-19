d389f1cc93928f05433a40dfb5451d578c94c64a
Ben Kempe <bk@onswipe.com>
2015-03-19 12:23:43 -0400

    just sqs test

--- src/main/scala/com/softwaremill/mqperf/Receiver.scala
+++ src/main/scala/com/softwaremill/mqperf/Receiver.scala
@@ -14,7 +14,7 @@ object Receiver extends App {
                          receiveMsgBatchSize = 10,
                          mqConfigMap = Map.empty)
 
-  for (numThreads <- Seq(50, 75)) {
+  for (numThreads <- Seq(3)) {
     val testConfig = testConfigWithThreads(numThreads)
     println(s"Starting test (receiver) with config: $testConfig")
 
--- src/main/scala/com/softwaremill/mqperf/mq/SqsMq.scala
+++ src/main/scala/com/softwaremill/mqperf/mq/SqsMq.scala
@@ -14,20 +14,24 @@ import com.amazonaws.regions.{Region, Regions}
 class SqsMq(configMap: Map[String, String]) extends Mq {
   def asyncClient = asyncBufferedClient
 
-  def asyncBufferedClient = asyncBufferedClientVal.get()
+  val asyncBufferedClient =
+    createClient()
+  //asyncBufferedClientVal.get()
 
+  def createClient() = {
+    val asyncClient = {
+      val c = new AmazonSQSAsyncClient(AWSCredentialsFromEnv(), new ClientConfiguration() withMaxConnections 3, Executors.newSingleThreadExecutor())
+      c.setRegion(Region.getRegion(Regions.US_EAST_1))
+      c
+    }
+    new AmazonSQSBufferedAsyncClient(asyncClient)
+  }
+/*
   private val asyncBufferedClientVal = new ThreadLocal[AmazonSQSBufferedAsyncClient] {
 
     override def initialValue(): AmazonSQSBufferedAsyncClient = {
-
-      val asyncClient = {
-        val c = new AmazonSQSAsyncClient(AWSCredentialsFromEnv(),new ClientConfiguration() withMaxConnections 3, Executors.newSingleThreadExecutor())
-        c.setRegion(Region.getRegion(Regions.US_EAST_1))
-        c
-      }
-      new AmazonSQSBufferedAsyncClient(asyncClient)
     }
-  }
+  }*/
 
   private val queueUrl = asyncClient.createQueue("mqperf-test-queue").getQueueUrl
 
