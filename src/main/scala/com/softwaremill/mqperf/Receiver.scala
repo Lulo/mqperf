c1722b9b0aabb4f86f2a097286c525e5ea11564c
Ben Kempe <bk@onswipe.com>
2015-03-19 16:39:41 -0400

    just sqs test

--- src/main/scala/com/softwaremill/mqperf/Receiver.scala
+++ src/main/scala/com/softwaremill/mqperf/Receiver.scala
@@ -14,7 +14,7 @@ object Receiver extends App {
                          receiveMsgBatchSize = 10,
                          mqConfigMap = Map.empty)
 
-  for (numThreads <- Seq(100)) {
+  for (numThreads <- Seq(20)) {
     val testConfig = testConfigWithThreads(numThreads)
     println(s"Starting test (receiver) with config: $testConfig")
 
